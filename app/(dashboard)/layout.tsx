caaf8633299607c373f071ef3c6f2f60d4a00165
retsambew <namanaskn2@gmail.com>
2023-08-18 12:12:12 +0530

    Auth

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <ClerkProvider>{children}</ClerkProvider>;
+}
