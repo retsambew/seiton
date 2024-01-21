4e08482e89c80be30a6ba572d92a412e21613458
retsambew <namanaskn2@gmail.com>
2024-01-22 00:56:42 +0530

    task cards modal

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,15 +1,19 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 
-export default function DashboardLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
+
+const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<ClerkProvider>
-			<Toaster />
-			{children}
+			<QueryProvider>
+				<Toaster />
+				<ModalProvider />
+				{children}
+			</QueryProvider>
 		</ClerkProvider>
 	);
-}
+};
+
+export default PlatformLayout;
